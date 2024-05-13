c034cfb74bede58ff3da8113535660b4206b3aba
Shubham <saishubhu200@gmail.com>
2024-05-14 00:07:57 +0530

    gussing game

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const username="javascript";
-let entered = prompt("Enter the UserName to login or Enter quit to exit");
+const num=prompt("enter the max range for random number generator");
+console.log(num)
+const random=Math.floor(Math.random()*num)+1;
+console.log(random)
 
-while(entered != username){
-    if(entered=="quit"){
-        console.log("you pressed quit");
+let gus=prompt("enter a gussing number");
+while(true){
+    if(gus=="quit"){
+        console.log("you presssed quit")
         break;
     }
-    confirm("Wrong UserName Please Input it again");
-    entered=prompt("Enter Username Again"); 
+    if(gus == random){
+        console.log("your are right")
+        break
     }
+    else{
+        gus=prompt("enter a gussing number");
+    }
+}
 
-if(entered == username){
-console.log("Welcome to your Website the username for this webpage is : ",username);
-}
-else{
-    console.log("You Entered Quit")
-}
+
