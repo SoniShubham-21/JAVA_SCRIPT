9763c6e1357480aa8729d187854cc53ce2e712f5
Shubham <saishubhu200@gmail.com>
2024-05-27 14:27:17 +0530

    this keyword example

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-const num=prompt("enter the max range for random number generator");
-console.log(num)
-const random=Math.floor(Math.random()*num)+1;
-console.log(random)
-
-let gus=prompt("enter a gussing number");
-while(true){
-    if(gus=="quit"){
-        console.log("you presssed quit")
-        break;
-    }
-    if(gus == random){
-        console.log("your are right")
-        break
-    }
-    else{
-        gus=prompt("enter a gussing number");
+const student={
+    name: "shubham",
+    age: 23,
+    eng: 25,
+    math: 45,
+    phy: 63,
+    getAvg(){
+        let avg1=(this.eng+this.math+this.phy)/3;
+        console.log(avg1);
     }
 }
-
-
+student.getAvg()
